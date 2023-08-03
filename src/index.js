f56d70630fc92756bcf57c0048947857f95073dd
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-08-03 03:23:46 -0400

    Successful GIF response created with search bar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import GetFetch from './fetch'
+import getFetch from './fetch';
 
 const loc = 'London';
 //let loc = prompt("Enter Location");
@@ -8,38 +8,34 @@ const weatherApiKey = 'da1ebd2eac854464a2443220230208';
 const weatherUrl = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${loc}`
 
 const giphyApiKey = 'fxGxrDaQt4YRxPcnwVmUfx5gGILgddK7';
-const giphyUrl = `https://api.giphy.com/v1/gifs/translate?api_key=${giphyApiKey}&s=dogs`
+
 const img = document.querySelector('img');
 
-// GetFetch(giphyUrl)
-//     .then(response => response.json())
-//     .then(handleGiphyResponse);
 
-// //handlers
 function handleWeatherResponse(newResponse) {
     latitude = newResponse.location.lat;
     console.log(latitude);
 }
 
-function handleGiphyResponse(newResponse){
+function handleGiphyResponse(newResponse) {
     img.src = newResponse.data.images.original.url;
-}   
+}
 
 
-// document.addEventListener('DOMContentLoaded', (event) => {    
-//     const btnGif = document.querySelector('#GIF-btn');
-//     btnGif.addEventListener('click', (event)=>{
-//         event.preventDefault();
-//         console.log("Button clicked");
-//     });
-// });
 
 
 const btnGif = document.getElementById('search-form');
-btnGif.addEventListener('submit', (event)=>{
+btnGif.addEventListener('submit', (event) => {
     event.preventDefault();
-    const inputValue = document.getElementById('GIF-search').value;
-    console.log(inputValue);
+    let gif = document.getElementById('GIF-search').value;
+    
+    const giphyUrl = `https://api.giphy.com/v1/gifs/translate?api_key=${giphyApiKey}&s=${gif}`;
+
+    //now use the giphyUrl to make a fetch request to the giphy api
+    getFetch(giphyUrl)
+        .then(response => response.json())
+        .then(handleGiphyResponse)
+        .catch(error => alert('GIF field cannot be empty'))
 
     //clear out the inout field after every button press
     document.getElementById('GIF-search').value = "";
