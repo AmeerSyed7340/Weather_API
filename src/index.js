1a215a5393c3c6f936b30303addb1fade1647a73
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-08-03 02:41:02 -0400

    Updated button response

--- src/index.js
+++ src/index.js
@@ -8,15 +8,14 @@ const weatherApiKey = 'da1ebd2eac854464a2443220230208';
 const weatherUrl = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${loc}`
 
 const giphyApiKey = 'fxGxrDaQt4YRxPcnwVmUfx5gGILgddK7';
-const giphyUrl = `https://api.giphy.com/v1/gifs/translate?api_key=${giphyApiKey}&s=cats`
+const giphyUrl = `https://api.giphy.com/v1/gifs/translate?api_key=${giphyApiKey}&s=dogs`
 const img = document.querySelector('img');
 
-console.log(loc);
-GetFetch(giphyUrl)
-    .then(response => response.json())
-    .then(handleGiphyResponse);
+// GetFetch(giphyUrl)
+//     .then(response => response.json())
+//     .then(handleGiphyResponse);
 
-//handlers
+// //handlers
 function handleWeatherResponse(newResponse) {
     latitude = newResponse.location.lat;
     console.log(latitude);
@@ -25,3 +24,23 @@ function handleWeatherResponse(newResponse) {
 function handleGiphyResponse(newResponse){
     img.src = newResponse.data.images.original.url;
 }   
+
+
+// document.addEventListener('DOMContentLoaded', (event) => {    
+//     const btnGif = document.querySelector('#GIF-btn');
+//     btnGif.addEventListener('click', (event)=>{
+//         event.preventDefault();
+//         console.log("Button clicked");
+//     });
+// });
+
+
+const btnGif = document.getElementById('search-form');
+btnGif.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const inputValue = document.getElementById('GIF-search').value;
+    console.log(inputValue);
+
+    //clear out the inout field after every button press
+    document.getElementById('GIF-search').value = "";
+})
