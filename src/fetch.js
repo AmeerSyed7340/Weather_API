8a0571a4c17fe583949118c13a9e051600718922
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-08-07 19:16:44 -0400

    Finished weather app

--- src/fetch.js
+++ src/fetch.js
@@ -1,5 +1,10 @@
-function getFetch(url){
-    return fetch(url)
+async function getFetch(url){
+    // return fetch(url).then(response => response.json())
+    const response = await fetch (url)
+    if(!response.ok){
+        throw new Error (`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
 }
 
 export default getFetch
--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ const loc = 'London';
 //let loc = prompt("Enter Location");
 
 const weatherApiKey = 'da1ebd2eac854464a2443220230208';
-const weatherUrl = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${loc}`
+const default_weatherUrl = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=New York`
 
 const giphyApiKey = 'fxGxrDaQt4YRxPcnwVmUfx5gGILgddK7';
+
 
 const img = document.querySelector('img');
 
@@ -21,22 +22,63 @@ function handleGiphyResponse(newResponse) {
     img.src = newResponse.data.images.original.url;
 }
 
+function updateDisplayInfo(condition, feelsLike_c, feelsLike_f, humidity, winds_mph, localTime){
+    const displayDiv = document.querySelector('.display-info');
+    displayDiv.innerHTML = `
+        <div><h1>Condition: ${condition} </h1></div>
+        <div><p>Feels like (C): ${feelsLike_c} </p></div>
+        <div><p>Feels like (F): ${feelsLike_f}</p></div>
+        <div><p>Humidity: ${humidity}</p></div>
+        <div><p>Winds (mph): ${winds_mph}</p></div>
+        <div><p>Local Time: ${localTime}</p></div>
+    `
+}
 
+async function callFetchAsync(weatherUrl) {
+    const weatherData = await getFetch(weatherUrl);
+    console.log(weatherData);
+    const condition = weatherData.current.condition.text;
+    const feelsLike_c = weatherData.current.feelslike_c;
+    const feelsLike_f = weatherData.current.feelslike_f;
+    const humidity = weatherData.current.humidity;
+    const winds_mph = weatherData.current.wind_mph;
+    const localTime = weatherData.location.localtime;
 
+    const location = weatherData.location.name;
+    const displayLocation = document.querySelector('.footer');
+    displayLocation.innerHTML = `<p>${location}</p>`
+
+    console.log(`${condition}, ${feelsLike_c}, ${feelsLike_f}, ${humidity}, ${winds_mph}, ${localTime}`);
+    updateDisplayInfo(condition, feelsLike_c, feelsLike_f,humidity, winds_mph, localTime);
+    
+    const giphyUrl = `https://api.giphy.com/v1/gifs/translate?api_key=${giphyApiKey}&s=${condition}`;
+    const gifData = await getFetch(giphyUrl);
+    handleGiphyResponse(gifData);
+}
+callFetchAsync(default_weatherUrl);
 
 const btnGif = document.getElementById('search-form');
-btnGif.addEventListener('submit', (event) => {
+btnGif.addEventListener('submit', async (event) => {
     event.preventDefault();
-    let gif = document.getElementById('GIF-search').value;
+    let location = document.getElementById('GIF-search').value;
     
-    const giphyUrl = `https://api.giphy.com/v1/gifs/translate?api_key=${giphyApiKey}&s=${gif}`;
-
+    //const giphyUrl = `https://api.giphy.com/v1/gifs/translate?api_key=${giphyApiKey}&s=${gif}`;
+    const weatherUrl = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${location}`
     //now use the giphyUrl to make a fetch request to the giphy api
-    getFetch(giphyUrl)
-        .then(response => response.json())
-        .then(handleGiphyResponse)
-        .catch(error => alert('GIF field cannot be empty'))
+    // const weatherData = await getFetch(weatherUrl);
+    // const condition = weatherData.current.condition.text;
+    // const feelsLike_c = weatherData.current.condition.feelslike_c;
+    // const feelsLike_f = weatherData.current.condition.feelslike_f;
+    // const humidity = weatherData.current.condition.humidity;
+    // const winds_mph = weatherData.current.condition.winds_mph;
+    // const localTime = weatherData.location.localtime;
+    
+    // updateDisplayInfo(condition, feelsLike_c, feelsLike_f,humidity, winds_mph, localTime);
+    // const giphyUrl = `https://api.giphy.com/v1/gifs/translate?api_key=${giphyApiKey}&s=${condition}`;
+    // const gifData = await getFetch(giphyUrl);
+    // handleGiphyResponse(gifData);
 
+    callFetchAsync(weatherUrl);
     //clear out the inout field after every button press
     document.getElementById('GIF-search').value = "";
 })
